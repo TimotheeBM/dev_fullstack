7792792282a0bd6b39b2f7ebe471617c58281ea4
ldrd <loki283@free.fr>
2019-03-03 17:41:22 +0100

    Authentification

--- routes/api.php
+++ routes/api.php
@@ -12,11 +12,23 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+/*
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+*/
 
-Route::get('events', function () {
-    return response(App\Event::all(),200);
+Route::get('events', 'EventsController@index');
+Route::get('events/{eventId}', 'EventsController@show');
+Route::post('events','EventsController@store');
+Route::put('events/{events}','EventsController@update');
+Route::delete('events/{eventId}', 'EventsController@delete');
+
+Route::post('login', 'PassportController@login');
+Route::post('register', 'PassportController@register');
+ 
+Route::middleware('auth:api')->group(function () {
+    Route::get('user', 'PassportController@details');
+ 
+    Route::resource('products', 'ProductController');
 });
