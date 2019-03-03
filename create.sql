fb51c4c171beaf60b0b3378801aa9917950ec70e
Timothée <tim.bm@hotmail.fr>
2019-03-03 14:52:10 +0100

    modification du script sql

--- create.sql
+++ create.sql
@@ -1,3 +1,11 @@
+create table if not exists Locations (
+    loc_id int auto_increment,
+    name varchar(255),
+    latitude float not null,
+    longitude float not null,
+    PRIMARY KEY (loc_id)
+);
+
 create table if not exists Users (
     user_id int auto_increment,
     pseudo varchar(12) unique not null,
@@ -5,20 +13,20 @@ create table if not exists Users (
     location int,
     PRIMARY KEY (user_id),
     CONSTRAINT fk_user_loc FOREIGN KEY (location) REFERENCES Locations(loc_id)
-)
+);
 
 create table if not exists Events (
     event_id int auto_increment,
     creator int not null,
     title varchar(30) not null,
-    descritption text,
+    description text,
     date_creation date not null,
     date_event date not null,
     location int,
     PRIMARY KEY (event_id),
     CONSTRAINT fk_event_user FOREIGN KEY (creator) REFERENCES Users(user_id),
     CONSTRAINT fk_event_loc FOREIGN KEY (location) REFERENCES Locations(loc_id)
-)
+);
 
 create table if not exists Messages (
     user_id int,
@@ -28,7 +36,7 @@ create table if not exists Messages (
     PRIMARY KEY (user_id, event_id, date),
     CONSTRAINT fk_message_user FOREIGN KEY (user_id) REFERENCES Users(user_id),
     CONSTRAINT fk_message_event FOREIGN KEY (event_id) REFERENCES Events(event_id)
-)
+);
 
 create table if not exists Guests (
     user_id int,
@@ -37,12 +45,4 @@ create table if not exists Guests (
     PRIMARY KEY (user_id, event_id),
     CONSTRAINT fk_guests_user FOREIGN KEY (user_id) REFERENCES Users(user_id),
     CONSTRAINT fk_guests_event FOREIGN KEY (event_id) REFERENCES Events(event_id)
-)
-
-create table if not exists Locations (
-    loc_id int auto_increment,
-    name varchar(255),
-    latitude float not null,
-    longitude float not null,
-    PRIMARY KEY (loc_id)
-)
+);
