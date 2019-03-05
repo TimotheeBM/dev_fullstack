bc55a8e75da0c90dcce37f34d827142e7da5fa32
TimotheeBM <tim.bm@hotmail.fr>
2019-03-05 13:59:50 +0100

    Merge partie 2

--- create.sql
+++ create.sql
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 create table if not exists locations (
-=======
-create table if not exists Locations (
->>>>>>> 18161577e68433d1295ebb7e05070f04c8f9728d
     loc_id int auto_increment,
     name varchar(255),
     latitude float not null,
@@ -10,11 +6,7 @@ create table if not exists Locations (
     PRIMARY KEY (loc_id)
 );
 
-<<<<<<< HEAD
 create table if not exists users (
-=======
-create table if not exists Users (
->>>>>>> 18161577e68433d1295ebb7e05070f04c8f9728d
     user_id int auto_increment,
     pseudo varchar(12) unique not null,
     password varchar(255) not null,
@@ -23,11 +15,7 @@ create table if not exists Users (
     CONSTRAINT fk_user_loc FOREIGN KEY (location) REFERENCES Locations(loc_id)
 );
 
-<<<<<<< HEAD
 create table if not exists events (
-=======
-create table if not exists Events (
->>>>>>> 18161577e68433d1295ebb7e05070f04c8f9728d
     event_id int auto_increment,
     creator int not null,
     title varchar(30) not null,
@@ -40,11 +28,7 @@ create table if not exists Events (
     CONSTRAINT fk_event_loc FOREIGN KEY (location) REFERENCES Locations(loc_id)
 );
 
-<<<<<<< HEAD
 create table if not exists messages (
-=======
-create table if not exists Messages (
->>>>>>> 18161577e68433d1295ebb7e05070f04c8f9728d
     user_id int,
     event_id int,
     date date,
@@ -54,11 +38,7 @@ create table if not exists Messages (
     CONSTRAINT fk_message_event FOREIGN KEY (event_id) REFERENCES Events(event_id)
 );
 
-<<<<<<< HEAD
 create table if not exists guests (
-=======
-create table if not exists Guests (
->>>>>>> 18161577e68433d1295ebb7e05070f04c8f9728d
     user_id int,
     event_id int,
     is_going boolean not null,
